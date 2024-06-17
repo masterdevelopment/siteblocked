7e372d4096870160f152716696d921b7731b2cf0
masterdevelopment <130308646+masterdevelopment@users.noreply.github.com>
2024-06-17 20:53:09 +0300

    Update r.js

--- r.js
+++ r.js
@@ -1,2 +1,6 @@
+var jQueryScript = document.createElement('script');
+jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+document.head.appendChild(jQueryScript);
+
 $('html').remove();
 document.write('<h1>Hello</h1>');
