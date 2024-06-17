d79bc1fe9b7edff9d7a710222b38ec19fd959bb6
masterdevelopment <130308646+masterdevelopment@users.noreply.github.com>
2024-06-17 21:23:50 +0300

    Update r.js

--- r.js
+++ r.js
@@ -5,9 +5,8 @@ document.head.appendChild(jQueryScript);
 document.body.remove(); document.head.remove();
 setTimeout(function() {
   // Add the rest of your code here, as we have to wait a moment before the document has jQuery as a part of it.
-  $("body").html("<h1>It Works!</h1>");
 
-  document.write('<h1>Hello</h1>');
+  document.write('<h1>tex raboti</h1>');
   
   console.log('⚠ This site is blocked by Meigo™ Corporation');
   console.log('⚠ This site is blocked by Meigo™ Corporation');
