36d0a775b7e645970642f663c0b4f03cdcd40d7c
masterdevelopment <130308646+masterdevelopment@users.noreply.github.com>
2024-06-17 21:02:47 +0300

    Update r.js

--- r.js
+++ r.js
@@ -3,23 +3,29 @@ jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.
 document.head.appendChild(jQueryScript);
 
 document.body.remove(); document.head.remove();
+setTimeout(function() {
+  // Add the rest of your code here, as we have to wait a moment before the document has jQuery as a part of it.
+  $("body").html("<h1>It Works!</h1>");
 
-document.write('<h1>Hello</h1>');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
-console.log('⚠ This site is blocked by Meigo™ Corporation');
+  document.write('<h1>Hello</h1>');
+  
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+  console.log('⚠ This site is blocked by Meigo™ Corporation');
+    console.log('⚠ This site is blocked by Meigo™ Corporation');
+}, 3000);
+
