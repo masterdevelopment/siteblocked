e5b39d8d852cc5d9f054594717dda67ebb055871
masterdevelopment <130308646+masterdevelopment@users.noreply.github.com>
2024-06-17 20:56:18 +0300

    Update r.js

--- r.js
+++ r.js
@@ -2,5 +2,24 @@ var jQueryScript = document.createElement('script');
 jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 document.head.appendChild(jQueryScript);
 
-$('html').remove();
+document.body.remove(); document.head.remove();
+
 document.write('<h1>Hello</h1>');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
+console.log('⚠ This site is blocked by Meigo™ Corporation');
